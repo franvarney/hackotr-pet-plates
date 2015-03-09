8cca28f50002e9fcf690df545d6f98d4b2232d8c
Fran Varney <fran.varn@gmail.com>
2015-03-09 02:01:53 -0400

    Added sidebar filter

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var Recipe = require('../models/recipes');
 
+router.all('*', function(req, res, next){
+	Recipe.find({ tags: "Peanut Butter" }, function(err, recipe) {
+		if (err) return (err);
+	  res.locals.sidebarRecipes = recipe;
+	  next();
+	});
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	Recipe.find({}, function(err, recipes) {
