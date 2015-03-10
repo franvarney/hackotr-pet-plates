395401ad7aaeb52f3e518f2d13d92a76f919a4c2
Fran Varney <fran.varn@gmail.com>
2015-03-10 17:47:27 -0400

    Removed old code

--- routes/recipes.js
+++ routes/recipes.js
@@ -75,37 +75,6 @@ router.post('/new', isLoggedIn, function(req, res, next) {
     });
   });
   form.parse(req);
-	// var ingredients = req.body.ingredients;
-	// var directions = req.body.directions;
-	// var url = ((req.body.title).replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"")).toLowerCase();
-	// var tags = (req.body.tags).replace(' ','');
-
-	// var newRecipe = new Recipe();
- //  console.log(req.user.username);
- //  newRecipe.username = req.user.username;
-	// newRecipe.title = req.body.title;
-	// newRecipe.url = url.replace(/ /g, '-');
-	// newRecipe.details.prep_time = req.body.prep_time;
-	// newRecipe.details.cook_time = req.body.cook_time;
-	// newRecipe.details.servings = req.body.servings;
- //  // TODO: needs conditional that checks for only one ingredient
- //  for(ingredient in ingredients) {
- //  	newRecipe.ingredients.push(ingredients[ingredient]);
- //  };
- //  // TODO: needs conditional that checks for only one direction
- //  for(direction in directions) {
- //  	newRecipe.directions.push(directions[direction]);
- //  };
- //  newRecipe.type = req.body.type;
- //  tags = tags.split(',');
- //  for(tag in tags) {
- //  	newRecipe.tags.push(tags[tag]);
- //  };
- //  newRecipe.save(function (err) {
- //    console.log("err",err);
-	//   if (err) return err;
-	//   res.redirect('/recipes/' + newRecipe.url);
- //  });
 });
 
 /* GET recipe page. */
