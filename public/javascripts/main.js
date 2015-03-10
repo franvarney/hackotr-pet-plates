5a07dd0f5e4c87ffd184bf82c31c90f3351c69fe
Fran Varney <fran.varn@gmail.com>
2015-03-10 17:16:22 -0400

    Clear text box value after clone
    + Some TODOs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,12 @@
 $(document).ready( function() {
 	$('.add-ingredient').click( function() {
-		$( ".ingredients ul li" ).last().clone().appendTo( ".ingredients ul" );
+		$(".ingredients ul li").last().clone().appendTo(".ingredients ul");
+		$(".ingredients ul li #ingredient").last().val('');
 	});
+	// TODO Add remove ingredient function
 	$('.add-direction').click( function() {
-		$( ".directions ul li" ).last().clone().appendTo( ".directions ul" );
+		$(".directions ul li").last().clone().appendTo(".directions ul");
+		$(".directions ul li #direction").last().val('');
 	});
+	// TODO Add remove direction function
 });
