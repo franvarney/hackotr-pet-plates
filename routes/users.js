f27c1f1d02f2b5bfde5a045a1fa8fcf99897ff64
Fran Varney <fran.varn@gmail.com>
2015-03-10 22:35:59 -0400

    Create basic user display page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users');
+var Recipe = require('../models/recipes');
+var multiparty = require('multiparty');
+var async = require('async');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET user page. */
+router.get('/:user', function(req, res, next) {
+	async.waterfall([
+	  function(callback){
+	    User.findOne({ username: req.params.user }, function(err, user) {
+		    if (err) return (err);
+		    callback(null, user);
+			});
+	  },
+	  function(user, callback){      
+	    Recipe.find({ username: req.params.user }, function(err, recipes) {
+		    if (err) return (err);
+		    callback(null, user, recipes);
+			});
+	  }], function (err, user, recipes) {
+	  	res.render('users/show', { title: "Needs title", userAccount: user, userRecipes: recipes});   
+	  }
+	);
+});
+
 
 module.exports = router;
