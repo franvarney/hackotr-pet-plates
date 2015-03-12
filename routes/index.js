52fad7f7b63e817e0721fd5b771e9c5b94a538ba
Fran Varney <fran.varn@gmail.com>
2015-03-12 13:58:56 -0400

    Add populate to query to retrieve user information based on user_id

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,13 @@ router.all('*', function(req, res, next){
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	Recipe.find({}, function(err, recipes) {
-			res.render('index', { title: 'Home', recipes: recipes })
+	Recipe.find().populate('user_id').exec( function(err, recipes) {
+		res.render('index', { title: 'Home', recipes: recipes })
 	});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { message: req.flash('loginMessage') }); 
+  res.render('login', { message: req.flash('loginMessage') });
 });
 
 module.exports = router;
