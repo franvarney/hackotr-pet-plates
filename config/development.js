ecd4542dbbb0ac68cda8e71d975a7a720bc22582
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2015-03-14 17:41:01 -0400

    set path for file uploads

--- config/development.js
+++ config/development.js
@@ -1,4 +1,8 @@
+var util = require('util');
+var path = require('path');
+
 module.exports = {
   db  : process.env.DB || 'mongodb://localhost/pet-plates-dev',
-  url : 'http://localhost:3000'
+  url : util.format('http://localhost:%s', process.env.PORT),
+  multiparty_upload_dir: process.env.MULTIPARTY_UPLOAD_DIR || path.resolve(__dirname, '..', 'public', 'uploads', 'recipes')
 };
